fa84269e196de7eaeb502e53739d61ad78d092a4
alan191006 <hdmquan@outlook.com>
2024-07-11 13:41:26 +1000

    Initial commit

---
+++ src/_data/client.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "name": "North Melbourne Web Designs",
+    "email": "design@northmelbourneweb.com",
+    "phoneForTel": "0486-010-43",
+    "phoneFormatted": "0486 010 043",
+    "address": {
+        "lineOne": "Kingsway Dr.",
+        "lineTwo": "",
+        "city": "Lalor",
+        "state": "VIC",
+        "zip": "3075",
+        // TODO: Map link
+        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+    },
+    "socials": {
+        "facebook": "https://www.facebook.com/",
+        "instagram": "https://www.instagram.com/",
+    },
+    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+    "domain": "https://www.northmelbourneweb.com"
+};
