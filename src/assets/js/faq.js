1b1fd667ca67dd994a8adb25eeb43fd3cf912739
alan191006 <hdmquan@outlook.com>
2024-07-25 00:41:31 +1000

    refactor(front page): Basic front page layout
    - Front page layout
    - Local icons
    - Primary color

---
+++ src/assets/js/faq.js
@@ -0,0 +1,9 @@
+const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
+for (const item of faqItems) {
+    const onClick = () => {
+    faqItems.forEach(faq => faq.classList.remove('active'));
+    item.classList.toggle('active')
+}
+item.addEventListener('click', onClick)
+}
+                                
