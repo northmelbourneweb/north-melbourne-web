efb71a575e87e60f18b170b14b96ca0fd4319707
Alan Huynh <hdmquan@outlook.com>
2024-07-26 13:49:47 +1000

    feat: Simple pricing page barebone

---
+++ src/_data/pricing.js
@@ -0,0 +1,45 @@
+module.exports = {
+    allServices: [
+      {
+        name: "Number of pages",
+        plans: [5, 9]
+      },
+      {
+        name: "Mobile-First, Responsive Design",
+        plans: ["check", "check"]
+      },
+      {
+        name: "Custom Coded Development, From Scratch",
+        plans: ["check", "check"]
+      },
+      {
+        name: "",
+        plans: ["check", "check"]
+      },
+      {
+        name: "",
+        plans: ["check", "check"]
+      },
+      {
+        name: "",
+        plans: ["check", "check"]
+      },
+      {
+        name: "",
+        plans: ["check", "check"]
+      },
+      {
+        name: "Profit and Loss Analysis",
+        plans: ["Available", "Available"]
+      },
+      {
+        name: "Spend Management",
+        plans: ["Available", "Available"]
+      }
+      // Add more service items here
+    ],
+    plans: [
+      { name: "Business (A$99/month)" },
+      { name: "Business Plus (A$179/month)" },
+    ]
+};
