d78d802a383708187017f0e1aedcf04bfd8d1a9f
alan191006 <hdmquan@outlook.com>
2024-07-27 16:14:26 +1000

    refactor: Edit pricing page details
    - Make pricing features functional
    - Change pricing banner design and images

--- src/_data/pricing.js
+++ src/_data/pricing.js
@@ -33,6 +33,10 @@ module.exports = {
         plans: ["check", "check"]
       },
       {
+        name: "Logo to SVG Conversion",
+        plans: ["check", "check"]
+      },
+      {
         name: "Contact Form Integration",
         plans: ["check", "check"]
       },
@@ -58,74 +62,78 @@ module.exports = {
       },
     ],
     lServices: [
-        {
-          name: "Number of Pages",
-          plans: [5, 9, 10]
+      {
+        name: "Number of Pages",
+        plans: [5, 9, 10]
+      },
+      {
+          name: "E-Commerce Capability",
+          plans: ["", "", "check"]
         },
-        {
-            name: "E-Commerce Capability",
-            plans: ["", "", "check"]
-          },
-        {
-          name: "Mobile-First, Responsive Design",
-          plans: ["check", "check", "check"]
-        },
-        {
-          name: "Custom Coded Development, From Scratch",
-          plans: ["check", "check", "check"]
-        },
-        {
-          name: "Unlimited Stock Images (As Needed)",
-          plans: ["check", "check", "check"]
-        },
-        {
-          name: "Unlimited Edits",
-          plans: ["", "", ""]
-        },
-        {
-          name: "Hosting and SSL Certificate",
-          plans: ["", "", ""]
-        },
-        {
-          name: "Domain",
-          plans: ["", "", ""]
-        },
-        {
-          name: "24/7 Support",
-          plans: ["", "", ""]
-        },
-        {
-          name: "Contact Form Integration",
-          plans: ["check", "check". "check"]
-        },
-        {
-          name: "Dark Mode",
-          plans: ["check", "check", "check"]
-        },
-        {
-          name: "Google Business Profile Setup and Maps Listing",
-          plans: ["check", "check", "check"]
-        },
-        // {
-        //   name: "Website Redesign Every",
-        //   plans: ["3 years", "2 years", "2 years"]
-        // },
-        {
-          name: "Content Management System",
-          plans: ["", "check", "check"]
-        },
-        {
-          name: "20% Discount on all Add-ons",
-          plans: ["", "check", "check"]
-        },
-      ],
+      {
+        name: "Mobile-First, Responsive Design",
+        plans: ["check", "check", "check"]
+      },
+      {
+        name: "Custom Coded Development, From Scratch",
+        plans: ["check", "check", "check"]
+      },
+      {
+        name: "Unlimited Stock Images (As Needed)",
+        plans: ["check", "check", "check"]
+      },
+      {
+        name: "Contact Form Integration",
+        plans: ["check", "check", "check"]
+      },
+      {
+        name: "Dark Mode",
+        plans: ["check", "check", "check"]
+      },
+      {
+        name: "Google Business Profile Setup and Maps Listing",
+        plans: ["check", "check", "check"]
+      },
+      {
+        name: "Logo to SVG Conversion",
+        plans: ["check", "check", "check"]
+      },
+      // {
+      //   name: "Website Redesign Every",
+      //   plans: ["3 years", "2 years", "2 years"]
+      // },
+      {
+        name: "Content Management System",
+        plans: ["", "check", "check"]
+      },
+      {
+        name: "20% Discount on all Add-ons",
+        plans: ["", "check", "check"]
+      },
+      {
+        name: "Unlimited Edits",
+        plans: ["", "", ""]
+      },
+      {
+        name: "Hosting and SSL Certificate",
+        plans: ["", "", ""]
+      },
+      {
+        name: "Domain",
+        plans: ["", "", ""]
+      },
+      {
+        name: "24/7 Support",
+        plans: ["", "", ""]
+      },
+    ],
     mPlans: [
       { name: "Business (A$99/month)" },
       { name: "Business Plus (A$179/month)" },
-    ]
+    ],
     lPlans: [
-        { name: "Business (A$2000)" },
-        { name: "Business Plus (A$3000)" },
-        { name: "E-Commerce (A$5000)" },
-      ]
+      { name: "Business (A$2000)" },
+      { name: "Business Plus (A$3000)" },
+      { name: "E-Commerce (A$5000)" },
+    ]
 };
