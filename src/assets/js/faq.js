27a4722c81ddcda58ff334837c818d496ee6a66c
alan191006 <hdmquan@outlook.com>
2024-07-30 07:54:28 +1000

    refactor: Restart for v5

--- src/assets/js/faq.js
+++
@@ -1,9 +0,0 @@
-const faqItems = Array.from(document.querySelectorAll('.faq-item'));
-for (const item of faqItems) {
-    const onClick = () => {
-    faqItems.forEach(faq => faq.classList.remove('active'));
-    item.classList.toggle('active')
-}
-item.addEventListener('click', onClick)
-}
-                                
