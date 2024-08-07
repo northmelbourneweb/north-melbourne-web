f8dd412478008224fbde311b44523877fbc226b0
alan191006 <hdmquan@outlook.com>
2024-08-07 11:01:53 +1000

    feat: Add Add-Ons section in Pricing page

---
+++ src/_data/addons.js
@@ -0,0 +1,20 @@
+module.exports = [
+    {
+        package: "Extra Pages",
+        price: "A$99",
+        description: "Per Page",
+        text: "Limited by our website packages? 9 pages not enough for you?\n\nYou can add additional pages at a fixed, one-time cost to your pre-existing package!\n\nAdditional pages are subject to the same Green Pagespeed Guarantee and Unlimited Edits services that we offer."
+    },
+    {
+        package: "Hosting",
+        price: "A$29",
+        description: "Per Month",
+        text: "For clients who buy a website from us through a 'lump sum' plan, we offer to host the website for you at an additional fee. Monthly subscription packages already have this included.\n\nWe boast a lightning-fast service on a global content delivery network. What's more, we promise a 99.98% uptime, free SSL certificate and unlimited backups with every edit requested."
+    },
+    {
+        package: "Logos and Branding",
+        price: "A$99",
+        description: "",
+        text: "Budding businesses taking their first steps need an emblem to represent themselves both on and off the internet.\n\nOur designer will work to create your logo. When finished, you'll have a logo in both PNG and SVG format, the editable source file, 4 concepts and 6 revisions during the design process."
+    },
+];
