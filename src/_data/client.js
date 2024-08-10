f60bfd19165cafa1ed7cfa45ad8d03b02a9490f8
alan191006 <hdmquan@outlook.com>
2024-08-10 21:08:39 +1000

    refactor: Add Google verify and edit sitemap

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,20 +1,20 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "North Melbourne Web",
+    "email": "design@northmelbourneweb.com",
+    "phoneForTel": "0486-010-043",
+    "phoneFormatted": "+61 (0) 486 010 043",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
-        "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
+        "lineOne": "",
+        "lineTwo": "",
+        "city": "Lalor",
+        "state": "VIC",
+        "zip": "3075",
+        "mapLink": "https://maps.app.goo.gl/3Uhn9eeN7WfXFeyM7"
     },
     "socials": {
         "facebook": "https://www.facebook.com/",
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://northmelbourneweb.com"
 };
