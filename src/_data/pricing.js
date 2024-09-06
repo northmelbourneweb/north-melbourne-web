866484c828a7c30a09dfba3f90a4faca5025be61
alan191006 <hdmquan@outlook.com>
2024-09-06 19:06:02 +1000

    feat: Templating Pricing component

---
+++ src/_data/pricing.js
@@ -0,0 +1,80 @@
+module.exports = {
+    monthly: [
+      {
+        plan: "Basic Plan",
+        price: "$199",
+        description: "Perfect for small businesses looking to establish a professional online presence.",
+        features: [
+          "Custom-coded website with up to 5 pages",
+          "Full analytics integration",
+          "Essential on-page SEO with local focus",
+          "Free logo (re)design & SVG conversion",
+          "Integrated blog setup"
+        ]
+      },
+      {
+        plan: "Professional Plan",
+        price: "$479",
+        description: "Ideal for growing businesses needing advanced features and enhanced SEO.",
+        features: [
+          "All features in the Basic Plan",
+          "Custom-coded website with up to 10 pages",
+          "Advanced SEO & local backlink strategy",
+          "CRM integration",
+          "1 custom blog post per month"
+        ]
+      },
+      {
+        plan: "Premium Plan",
+        price: "$999",
+        description: "Comprehensive package for businesses ready to dominate their market.",
+        features: [
+          "All features in the Professional Plan",
+          "Fully custom-coded website with unlimited pages",
+          "Extensive SEO & ongoing backlinking strategy",
+          "CRM & email marketing integration",
+          "Premium blog content & monthly high-detail illustrations"
+        ]
+      }
+    ],
+    oneOff: [
+      {
+        plan: "Standard Plan",
+        price: "$5488",
+        description: "A custom website with up to 10 pages, local SEO, an integrated blog, a free logo redesign, and top-notch performance optimization.",
+        features: [
+            "Custom-coded website with up to 10 pages.",
+            "Essential on-page SEO with a local focus.",
+            "Integrated blog setup.",
+            "Free logo (re)design & SVG conversion."
+        ]
+      },
+      {
+        plan: "E-Commerce Plan",
+        price: "$9828",
+        description: "All the features of the Standard Plan plus full e-commerce functionality, including product catalog setup, payment gateway integration, and basic product SEO.",
+        features: [
+          "Includes all features of the Standard Plan.",
+            "E-commerce integration (WooCommerce or Shopify).",
+            "Product catalog setup for up to 50 products.",
+            "Payment gateway integration.",
+            "Shopping cart and checkout setup.",
+            "Product SEO (title tags, meta descriptions, schema markup).",
+        ]
+      },
+      {
+        plan: "SaaS Plan",
+        price: "$12884",
+        description: "The Standard Plan features plus user authentication, subscription management, advanced customizations, and API integrations for SaaS platforms.",
+        features: [
+            "Includes all features of the Standard Plan.",
+            "User authentication (logins, account management).",
+            "Subscription payment setup (Stripe, PayPal).",
+            "Basic membership portal or restricted content areas.",
+            "Advanced customizations (dashboards, user tiers).",
+            "API integrations (payment processing or other services)."
+        ]
+      }
+    ]
+  };
+  
