d1ed763991f55ab9ba4f6460f9d4a94af7874f24
alan191006 <hdmquan@outlook.com>
2024-09-06 15:24:14 +1000

    fix: Wrong URL for project section

--- src/_data/projects.js
+++ src/_data/projects.js
@@ -3,13 +3,13 @@ module.exports = [
       title: "North Melbourne Web (old)",
       category: "Web Development",
       imageUrl: "/assets/images/project/North Melbourne Web mockup.jpg",
-      detailsUrl: "project-details.html",
+      detailsUrl: "https://66cd46de6b0f30000880db6f--venerable-seahorse-36b4a2.netlify.app/",
     },
     {
       title: "Leafe Landscaping",
       category: "Web Development",
       imageUrl: "/assets/images/project/Leafe Landscaping mockup.jpg",
-      detailsUrl: "project-details.html",
+      detailsUrl: "https://leafe-landscaper.netlify.app/",
     },
   ];
   
