f8f51742691fd7b491342c15bd151997111fc4f9
alan191006 <hdmquan@outlook.com>
2024-10-01 14:25:45 +1000

    fix(?): Netlify

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,13 @@
-
+if (window.netlifyIdentity) {
+    window.netlifyIdentity.on("init", (user) => {
+      if (!user) {
+        window.netlifyIdentity.on("login", () => {
+          document.location.href = "/admin/";
+        });
+      }
+    });
+  }
+  
 (function ($) {
     "use strict";
 
